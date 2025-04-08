d7394e3c9595ee0bc190098120fce50ca3dac7fb
Daniel Camargo <daniel.camargodelima2@gmail.com>
2025-04-08 11:44:09 -0300

    pronto projeto vingadores

--- auxlogin.php
+++ auxlogin.php
@@ -15,8 +15,29 @@ $consultaUsuarioSenha = 'SELECT * FROM tb_usuario WHERE usuario = "' . $userForm
 
 $resultado = $banco->query($consultaUsuarioSenha)->fetch();
 
-var_dump($resultado);
+$status = $resultado['status'];
+?>
 
+
+<?php if ($status == 'admin') { ?>
+
+<h1>Bem vindo USUARIO ADMIN</h1>
+
+<?php } ?>
+
+<h1>Bem vindo USUARIO COMUM</h1>
+
+<a href="#" class="btn btn-success">Cadastrar -comum</a>
+
+<a href="#" class="btn btn-primary">Abrir -comum</a>
+
+<a href="#" class="btn btn-warning <?= $status != 'admin' ? 'disable' : ''  ?>" >Editar -admin</a>
+
+<a href="#" class="btn btn-danger">Excluir -admin</a>
+
+
+<?php
+die;
 if (!empty($resultado) && $resultado != false){
     header('location:index.php');
 } else {
